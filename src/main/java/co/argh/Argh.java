9b02a9c2cba71d818e377be8151070d6dc1c6ef5
dannowilby <dywilby@gmail.com>
2019-06-30 19:27:59 -0700

    initial setup

---
+++ src/main/java/co/argh/Argh.java
@@ -0,0 +1,30 @@
+package co.argh;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Argh.MODID)
+public class Argh {
+
+	public static final String NAME = "Argh";
+	public static final String MODID = "argh";
+	public static final String VERSION = "1.0";
+	
+	@EventHandler
+	public void preinit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public void postinit(FMLPostInitializationEvent event) {
+		
+	}
+}
